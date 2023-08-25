ddd366fe1366362b75758ed519c46412ae72123f
RyuuYou <ryuuyou0529@gmail.com>
2023-08-25 13:41:52 +0800

    update

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
-  HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
+  HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
   HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
   // 是否显示开始阅读按钮
@@ -31,8 +31,8 @@ const CONFIG = {
   WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   WIDGET_ANALYTICS: false, // 显示统计卡
   WIDGET_TO_TOP: true,
-  WIDGET_TO_COMMENT: true, // 跳到评论区
-  WIDGET_DARK_MODE: true, // 夜间模式
+  WIDGET_TO_COMMENT: false, // 跳到评论区
+  WIDGET_DARK_MODE: false, // 夜间模式
   WIDGET_TOC: true // 移动端悬浮目录
 }
 export default CONFIG
